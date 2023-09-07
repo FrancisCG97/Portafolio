e24d90d48531ef98326bc65d8283ed24d565c3fe
Francis <f.carrasco.g.07@gmail.com>
2023-09-07 12:35:37 -0300

    First commit

---
+++ src/Components/body.jsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
+
+function Body() {
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Body;
---
+++ src/Home.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Body from './Components/body';
+import Principal from './Components/principal';
+
+function Home() {
+
+  return (
+    <>
+    <Principal></Principal>
+    <Body></Body>
+    </>
+  )
+}
+
+export default Home;
